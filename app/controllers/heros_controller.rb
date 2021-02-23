7309e9e17df04346b7299a05b6af0a9b9ec04700
Edward Pina <en.pina1@gmail.com>
2021-02-23 18:03:14 +0100

    show

---
+++ app/controllers/heros_controller.rb
@@ -0,0 +1,40 @@
+class HerosController < ApplicationController
+  before_action :set_hero, only: [:show, :edit, :update, :destroy]
+  def index
+    # Hero.all
+    @heros = policy_scope(Hero)
+  end
+
+  def show
+    set_hero
+  end
+
+  def new
+    @hero = Hero.new
+    authorize @hero
+  end
+
+  def create
+    # @user = User.find(params[:user_id])
+    @hero = Hero.new(hero_params)
+    @hero.user = current_user
+    if @hero.save
+      redirect_to hero_path(@hero)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def hero_params
+    params.require(:hero).permit(:name, :description, :price, :skill)
+  end
+
+  def set_hero
+    @hero = Hero.find(params[:id])
+    authorize @hero
+  end
+end
+
+
