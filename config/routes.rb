f24e7e760b410bfb361f69f76b48572e453ef492
Edward Pina <en.pina1@gmail.com>
2021-02-23 19:55:08 +0100

    everything working

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'heros', to: 'heros#index'
-  post 'heros', to: 'heros#create'
-  get 'heros/new', to: 'heros#new', as: 'new_hero'
-  get 'heros/:id/edit', to: 'heros#edit', as: 'edit_hero'
-  get 'heros/:id', to: 'heros#show'
-  patch 'heros/:id', to: 'heros#update'
-  put 'heros/:id', to: 'heros#update'
-  delete 'heros/:id', to: 'heros#destroy', as: 'delete_hero'
+  # get 'heros', to: 'heros#index'
+  # post 'heros', to: 'heros#create'
+  # get 'heros/new', to: 'heros#new', as: 'new_hero'
+  # get 'heros/:id/edit', to: 'heros#edit', as: 'edit_hero'
+  # get 'heros/:id', to: 'heros#show'
+  # patch 'heros/:id', to: 'heros#update'
+  # put 'heros/:id', to: 'heros#update'
+  # delete 'heros/:id', to: 'heros#destroy', as: 'delete_hero'
+  resources :heros
 end
