eb607c6639113c8b25d2044252291927b768c582
Cynthia Herberg <cynthia_herberg@hotmail.de>
2021-02-24 15:55:52 +0100

    controller half setup and create method

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,29 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+
+  def show?
+    user_is_owner_or_admin?
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+  end
+
+  def destroy?
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    user == record.user || user.admin == true
+  end
+end
